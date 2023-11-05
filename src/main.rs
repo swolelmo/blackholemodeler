6b4fbf221fa4c1aa7fda211f7efbd6bbad1cd169
Micah Verwey <micahverwey@microsoft.com>
2023-11-04 19:31:35 -0700

    fix

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ fn main() {
 
             println!("Serializing to file: {0}", &args[2]);
 
-            model.serialize_to_file(&args[2]);
+            model.serialize_to_json(&args[2]);
         }
         else {
             println!("Failed to load AiScene for file: {0}", &args[1]);
@@ -49,22 +49,15 @@ fn are_args_valid(args: &Vec<String>) -> bool {
 }
 
 unsafe fn process_node(node: *mut AiNode, scene: *const AiScene) -> Vec<Mesh> {
-    println!("Processing Node");
-
     let mut to_return = Vec::new();
     for i in 0..(*node).num_meshes as isize {
         let mesh_index = (*(*node).meshes.offset(i)) as isize;
-        println!("index: {0}, mesh index: {1}, num meshes: {2}, num node meshes: {3}", i, mesh_index, (*scene).num_meshes, (*node).num_meshes);
         let ai_mesh = (*(*scene).meshes).offset(mesh_index);
         if !ai_mesh.is_null() {
             to_return.push(process_mesh(ai_mesh));
         }
-        else {
-            println!("Empty Mesh");
-        }
     }
     
-    println!("Processing Child Nodes");
     for i in 0..(*node).num_children as isize {
         to_return.append(&mut process_node(*(*node).children.offset(i), scene));
     }
@@ -73,18 +66,13 @@ unsafe fn process_node(node: *mut AiNode, scene: *const AiScene) -> Vec<Mesh> {
 }
 
 unsafe fn process_mesh(mesh: *const AiMesh) -> Mesh {
-    println!("Processing Mesh");
-
     let mut vertices = Vec::new();
     for i in 0..(*mesh).num_vertices as isize {
-        println!("Processing Position");
         let position = Vec3 { x: (*(*mesh).vertices.offset(i)).x, y: (*(*mesh).vertices.offset(i)).y, z: (*(*mesh).vertices.offset(i)).z };
-        println!("Processing TexturePos");
         let tex_position = Vec2 { x: (*(*mesh).texture_coords[0].offset(i)).x, y: (*(*mesh).texture_coords[0].offset(i)).y };
         vertices.push(TextureVertex { position, tex_position });
     }
 
-    println!("Creating Mesh");
     let mut indices = Vec::new();
     let mut textures = Vec::new();
     Mesh::new(vertices, indices, textures)
